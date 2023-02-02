604608b2e911a0faa1d92adaf39321c35585d142
jinyoung0305 <jinyeong0305@naver.com>
2023-02-02 21:02:53 +0900

    react-data-app

--- public-data-app/src/App.js
+++ public-data-app/src/App.js
@@ -1,7 +1,7 @@
 import React, {useState, useEffect} from 'react'
 
 function fetchData() {
-  const promise = fetch("요청할 주소")
+  const promise = fetch("https://apis.data.go.kr/B552061/frequentzoneBicycle/getRestFrequentzoneBicycle?serviceKey=O9LgAKxzZj3cJj949bIMCERmM1tsOGq4BbZ%2BR%2BdleIHB%2BiSsEdmADXsEwKPfB7zNBIDusKvcFUw6%2Fjlx77kHJg%3D%3D&searchYearCd=2021&siDo=11&guGun=110&type=json&numOfRows=10&pageNo=1")
     .then(res => {
       if(!res.ok) {
         throw res;
